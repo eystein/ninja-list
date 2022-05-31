6cfae5cd8135428c792d67aa0753f0f24f059371
eystein <eystein1@gmail.com>
2022-05-31 15:20:31 +0200

    Lessons 1 and 2. Add stateless components / pages.

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+// Stateless functional component
+const About = () => {
+	return (
+		<>
+			<h1>About</h1>
+			<p>This is my first attempt at a stateless functional component.</p>
+			<p>Or, you know, an About webpage. Booh!</p>
+			<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam nulla voluptate saepe necessitatibus nostrum alias tempora, libero distinctio facilis excepturi, amet quisquam sequi nobis modi eveniet numquam maiores ad! Vero?</p>
+		</>
+	);
+}
+ 
+export default About;
+
---
+++ pages/ninjas/index.js
@@ -0,0 +1,9 @@
+const Ninjas = () => {
+	return ( 
+		<>
+			<h1>Ninjas list</h1>
+		</>
+	);
+}
+ 
+export default Ninjas;
