edf1d6aa58f5531274b48b713d1d6f818b0fa011
eystein <eystein1@gmail.com>
2022-05-31 21:55:55 +0200

    Lesson 4: create Layout component

--- pages/index.js
+++ pages/index.js
@@ -7,15 +7,13 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-		<>
-			<Navbar />
+		<>			
 			<h1>Hello next</h1>
 			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore officiis assumenda pariatur eos illum, sunt sequi aut atque consequuntur dolorem recusandae quia suscipit omnis laboriosam architecto! Nam quaerat dolore blanditiis.</p>
 			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore officiis assumenda pariatur eos illum, sunt sequi aut atque consequuntur dolorem recusandae quia suscipit omnis laboriosam architecto! Nam quaerat dolore blanditiis.</p>
 			<Link href="/ninjas">
 				<a>See Ninja listing</a>
 			</Link>
-			<Footer />
 		</>
   )
 }
