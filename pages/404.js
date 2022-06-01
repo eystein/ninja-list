2f56000165541902ed73be0c46c4246723af8afa
eystein <eystein1@gmail.com>
2022-06-01 12:00:15 +0200

    Lesson 8: Add automatic Routing

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,21 @@
 import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const NotFound = () => {
+	const router = useRouter();
+
+	// Fire this function when the page first loads.
+	useEffect(() => {
+		setTimeout(() => {
+			// router.go(-1)
+			router.push('/'); // redirect to homepage.
+		}, 3000) // after 3 seconds.
+	}, []) // empty array as second argument
+	// the dependency array
+	// when empty, says only fire this function once
+	// and not again if any state changes
+
 	return (
 		<div className="not-found">
 			<h1>Oooops...</h1>
