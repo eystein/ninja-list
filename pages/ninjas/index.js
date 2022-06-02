e3472d7d0f59003329a843a4d31a36359136ae1b
eystein <eystein1@gmail.com>
2022-06-02 13:11:35 +0200

    Lesson 10: fetch and use JSON data with `getStaticProps`.

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,7 +1,27 @@
-const Ninjas = () => {
+import styles from '../../styles/Ninjas.module.css';
+
+export const getStaticProps = async () => {
+
+	const res = await fetch('https://jsonplaceholder.typicode.com/users');
+	const data = await res.json();
+
+	return {
+		props: { ninjas: data }
+	}
+
+}
+
+const Ninjas = ({ ninjas }) => {
 	return ( 
 		<>
 			<h1>Ninjas list</h1>
+			{ninjas.map(ninja => (
+				<div key={ninja.id}>
+					<h3>
+						<a className={styles.single}>{ ninja.name }</a>
+					</h3>
+				</div>
+			))}
 		</>
 	);
 }
