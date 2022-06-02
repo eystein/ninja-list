86d12b3a7d9755e6e077cd34c4cacbfc7994fdf2
eystein <eystein1@gmail.com>
2022-06-02 22:57:50 +0200

    Lesson 12: use `getStaticPaths` to create routes

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,3 +1,22 @@
+export const getStaticPaths = async () => {
+	const res = fetch('https://jsonplaceholder.typicode.com/users');
+	const data = await res.json();
+
+	const path = data.map(ninja => {
+		return {
+			params: {
+				id: ninja.id.toString()
+			}
+		}
+	})
+
+	return {
+		// paths: [{}, {}, { params: { id: 1} }]
+		paths: paths,
+		fallback: false
+	}
+}
+
 const Details = () => {
 	return (
 		<div>
