9a9bf486f83da0652ce8e0359548aca04fe274ef
eystein <eystein1@gmail.com>
2022-06-03 10:36:05 +0200

    Lesson 13: Fetch and display dynamic data for single page.

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -2,25 +2,36 @@ export const getStaticPaths = async () => {
 	const res = fetch('https://jsonplaceholder.typicode.com/users');
 	const data = await res.json();
 
+	// map data to an array of path objects with params (id)
 	const path = data.map(ninja => {
 		return {
-			params: {
-				id: ninja.id.toString()
-			}
+			params: { id: ninja.id.toString() }
 		}
 	})
 
 	return {
-		// paths: [{}, {}, { params: { id: 1} }]
-		paths: paths,
+		paths,
 		fallback: false
 	}
 }
 
-const Details = () => {
+export const getStaticProps = async (context) => {
+	const id = context.params.id;
+	const res =  await fetch('https://jsonplaceholder.typicode.com/users' + id);
+	const data = await res.json();
+
+	return {
+		props: { ninja: data }
+	}
+}
+
+const Details = ( { ninja }) => {
 	return (
 		<div>
-			<h1>Details page</h1>
+			<h1>{ ninja.name }</h1>
+			<p>{ ninja.email }</p>
+			<p>{ ninja.website }</p>
+			<p>{ ninja.address.city }</p>
 		</div>
 	);
 }
